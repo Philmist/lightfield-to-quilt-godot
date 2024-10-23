347b44d1deaef281f613f0bbc5944cba6534680e
philmist <philmist.j@gmail.com>
2024-10-23 17:42:06 +0900

    Add "enter_orchestration" but not tested.
    * Add manupulating orchestration function to lkg_bridge_node.gd
    * Add "GitPlugin" addon.

--- LKGBridge/lkg_bridge_node.gd
+++ LKGBridge/lkg_bridge_node.gd
@@ -20,7 +20,7 @@ func test_is_bridge_alive() -> bool:
 		return false
 	var err := http.request(LKG_HOST)
 	if err != OK:
-		printerr("Something wrong with HTTP request. [%s]" % [err])
+		printerr("Something wrong with HTTP request ping. [%s]" % [error_string(err)])
 	return true
 
 signal bridge_alive_checked(result: bool)
@@ -38,20 +38,71 @@ func _http_is_bridge_alive(result, response_code, _headers, _body):
 		bridge_alive_checked.emit(true)
 
 func enter_orchestration() -> bool:
-	if http:
+	if !_http_request_helper(_process_enter_orchestration):
+		enter_orchestration_failed.emit()
 		return false
+	var json_str = JSON.stringify({
+		"name": orchestration_string
+	})
+	var err := http.request(
+		LKG_HOST + "enter_orchestration",
+		PackedStringArray(),
+		HTTPClient.METHOD_POST,
+		json_str
+		)
+	if err != OK:
+		printerr("Something wrong with HTTP request to enter orchestration. [%s]" % [error_string(err)])
 	return true
 
-signal enter_orchestration_succeeded
+signal enter_orchestration_succeeded(status: String)
+signal enter_orchestration_failed
 
 func _process_enter_orchestration(result, response_code, _headers, body):
 	http.request_completed.disconnect(_process_enter_orchestration)
 	http.queue_free()
 	http = null
 	if result != HTTPRequest.Result.RESULT_SUCCESS:
-		printerr("Failed to enter orchestration.")
+		printerr("Failed to enter orchestration (failed to send http request).")
+		enter_orchestration_failed.emit()
 		return
 	if response_code != 200:
 		printerr("Failed to enter orchestration: HTTP code %d" % response_code)
+		enter_orchestration_failed.emit()
 		return
-	var json = JSON.parse_string(body.get_string_from_utf8())
+	var json_obj = JSON.parse_string(body.get_string_from_utf8()) as Dictionary
+	if json_obj == null:
+		printerr("Parsing json failed.")
+		enter_orchestration_failed.emit()
+		return
+	elif typeof(json_obj) != TYPE_DICTIONARY:
+		printerr("enter_orchestration doesn't return dictionary.")
+		enter_orchestration_failed.emit()
+		return
+	elif not ("payload" in json_obj and "status" in json_obj):
+		printerr("something wrong with orchestration json")
+		printerr(json_obj)
+		enter_orchestration_failed.emit()
+		return
+	_orchestration_token = String(json_obj["payload"]["value"])
+	var status = String(json_obj["status"]["value"])
+	enter_orchestration_succeeded.emit(status)
+	return
+
+func exit_orchestration() -> bool:
+	if !_http_request_helper(_process_exit_orchestration):
+		printerr("Can't exit orchetration because creating http request is failed.")
+		return false
+	var json_str := JSON.stringify({
+		"orchestration": _orchestration_token
+	})
+	var err := http.request(
+		LKG_HOST + "exit_orchestration"
+	)
+	return true
+
+func _process_exit_orchestration(_result, _code, _headers, _body):
+	http.request_completed.disconnect(_process_exit_orchestration)
+	_orchestration_token = ""
+	http.queue_free()
+	http = null
+	return
